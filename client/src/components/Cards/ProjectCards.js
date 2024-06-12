b2c5355f136ecaf02ea8e3881343421bb76cc205
RyanMalloy <129435196+RyanMalloy@users.noreply.github.com>
2024-06-11 21:55:56 -0400

    eslint

--- client/src/components/Cards/ProjectCards.js
+++ client/src/components/Cards/ProjectCards.js
@@ -2,11 +2,8 @@ import React from "react";
 import ProjectCard from "./ProjectCard";
 import ProjectData from "../../assets/data/ProjectData";
 import { Box } from "@mui/material";
-import { useMediaQuery } from "react-responsive";
 
 const ProjectCards = () => {
-  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
-
   return (
     <>
       <Box id="project-cards" sx={{}}>
